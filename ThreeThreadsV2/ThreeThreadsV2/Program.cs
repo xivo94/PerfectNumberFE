073df4d57fbe9b2b9deb35dfb77b99589ad9a1ba
xivo94 <72750328+xivo94@users.noreply.github.com>
2020-10-12 17:00:47 +0200

    Thread Test v2
    Added Test v2

---
+++ ThreeThreadsV2/ThreeThreadsV2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+
+
+class TwoThreads
+{
+    static void Main()
+    {
+        new Thread(Go).Start();      // Call Go() on a new thread
+        new Thread(Walk).Start();
+        new Thread(newF).Start();
+        //Go();                        // Call Go() on the main threa
+
+
+
+        Console.ReadKey();
+
+    }
+
+    static void Go()
+    {
+        // Declare and use a local variable - 'cycles'
+        for (int cycles = 0; cycles < 3000; cycles++) Console.Write('?');
+    }
+
+    static void Walk()
+    {
+        // Declare and use a local variable - 'cycles'
+        for (int cycles = 0; cycles < 1000; cycles++) Console.Write('-');
+    }
+
+    static void newF()
+    {
+        for(int i= 0; i< 3500; i++)
+        {
+            Console.Write("*");
+        }
+    }
+
+}
